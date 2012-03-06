fb682512351c90062cc2881ffb5fa9475497e477
Mikhail Krasilnikov <mihalych@vsepofigu.ru>
2012-03-06 19:26:31 +0400

    Добавлены классы.

---
+++ lib/ORMBundle/ORMBundle.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Компонент Eresus ORM для  Symfony2
+ *
+ * @version 1.0
+ *
+ * @copyright 2011, Михаил Красильников, <mihalych@vsepofigu.ru>
+ *
+ * @package ORMBundle
+ * @author Михаил Красильников <mihalych@vsepofigu.ru>
+ */
+
+namespace Eresus\ORMBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Компонент ORM
+ *
+ * @package ORMBundle
+ * @since 1.0
+ */
+class ORMBundle extends Bundle
+{
+}
