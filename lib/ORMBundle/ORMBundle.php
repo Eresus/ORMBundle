2248c4f278c438bec2b9174492a7e082e8f56ff8
Mikhail Krasilnikov <mihalych@vsepofigu.ru>
2012-03-07 13:51:01 +0400

    Теперь пакету задаётся уникальное имя.

--- lib/ORMBundle/ORMBundle.php
+++ lib/ORMBundle/ORMBundle.php
@@ -2,7 +2,7 @@
 /**
  * Компонент Eresus ORM для  Symfony2
  *
- * @version 1.0
+ * @version 1.0.1
  *
  * @copyright 2011, Михаил Красильников, <mihalych@vsepofigu.ru>
  *
@@ -22,4 +22,17 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ORMBundle extends Bundle
 {
+	/**
+	 * Конструктор
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.1
+	 */
+	public function __construct()
+	{
+		// Задаём уникальное имя, чтобы избежать конфликта с другими пакетами
+		$this->name = 'EresusORMBundle';
+	}
+	//-----------------------------------------------------------------------------
 }
